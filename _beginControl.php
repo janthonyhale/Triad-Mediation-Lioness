28aeaa12446ec4e5086b18ea6bcb939f746aa830
EC2 Default User <ec2-user@ip-172-31-8-172.us-west-1.compute.internal>
2025-05-08 10:58:51 +0000

    Empty

--- _beginControl.php
+++
@@ -1,6 +0,0 @@
-<?php
-include('_projectID.php');
-include(PATH . "basis/common.php");
-$_SESSION['projectID'] = PROJECTID;
-include('_stageNames.php');
-include(PATH . "basis/controlpanel/control.php");
